eb97c573e5260cfff3f577c7fe5d5377bffa047f
wanyang3 <wanyang3@staff.weibo.com>
2018-06-13 18:00:55 +0800

    modify main.go

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	// 设置显示该目录下所有的文件列表
-	beego.BConfig.WebConfig.DirectoryIndex = true
-	// 设置静态文件访问路径，也可以在 app.conf 中配置
-	beego.SetStaticPath("/download", "download")
+	if beego.BConfig.RunMode == "dev" {
+		// 设置显示该目录下所有的文件列表
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		// 设置静态文件访问路径，也可以在 app.conf 中配置
+		beego.SetStaticPath("/download", "download")
+		//beego.BConfig.WebConfig.StaticDir["/download"] = "download"
+	}
 	// 设置 Controller 方式定义 Error 错误处理函数
 	beego.ErrorController(&controllers.ErrorController{})
 	beego.Run()
